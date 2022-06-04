50f888f6d82c71eff47bfaf2cab4ea5ad3618bb8
unknown <heringcoelho@gmail.com>
2022-06-04 08:28:56 -0300

    ER4

--- Classes/PessoaJuridica.cs
+++ Classes/PessoaJuridica.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using cadastropessoauc12.Interfaces;
 
 namespace cadastropessoauc12.Classes
@@ -8,17 +9,48 @@ namespace cadastropessoauc12.Classes
         public string? Cnpj { get; set; }
 
         public string? RazaoSocial { get; set; }
-        
-                     
+
+
 
         public override float PagarImposto(float rendimento)
         {
             throw new NotImplementedException();
         }
 
+
+
+        //XX.XXX.XXX/0001-xx ----- XXXXXXXX0001XX
         public bool ValidarCnpj(string cnpj)
         {
-            throw new NotImplementedException();
+
+            // = Atribuição
+            // == Comparação (igualdade)
+            // === Comparação exata (igualdade de valor e tipo)
+            bool retornoCnpjValido = Regex.IsMatch(cnpj, @"(^(\d{2}.\d{3}.\d{3}/\d{4}-d{2})|(\d{14})$)");
+
+            //if (Regex.IsMatch(cnpj, @"(^(\d{2}.\d{3}.\d{3}/\d{4}-d{2})|(\d{14})$)"))
+            if (retornoCnpjValido == true)
+            {
+                if (cnpj.Length == 18)
+                {
+                    string subStringCnpj = cnpj.Substring(11, 4);
+
+                    if (subStringCnpj == "0001")
+                    {
+                        return true;
+                    }
+                }
+                else if (cnpj.Length == 14)
+                {
+                    string subStringCnpj = cnpj.Substring(8, 4);
+
+                    if (subStringCnpj == "0001")
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,28 +1,51 @@
 ﻿using cadastropessoauc12.Classes;
 
-PessoaFisica novaPf = new PessoaFisica();
-PessoaFisica metodosPf = new PessoaFisica();
-Endereco novoEndPf = new Endereco();
+// PessoaFisica novaPf = new PessoaFisica();
+// PessoaFisica metodosPf = new PessoaFisica();
+// Endereco novoEndPf = new Endereco();
 
-novaPf.Nome = "Alles Blau";
-novaPf.dataNasc = new DateTime(2000, 01, 01);
-novaPf.Cpf = "1234567890";
-novaPf.Rendimento = 3500.5F;
+// novaPf.Nome = "Alles Blau";
+// novaPf.dataNasc = new DateTime(2000, 01, 01);
+// novaPf.Cpf = "1234567890";
+// novaPf.Rendimento = 3500.5F;
 
-novoEndPf.logradouro = "Rua das Flores";
-novoEndPf.numero = 594;
-novoEndPf.complemento = "Livraria Globo";
-novoEndPf.endComercial = true;
+// novoEndPf.logradouro = "Rua das Flores";
+// novoEndPf.numero = 594;
+// novoEndPf.complemento = "Livraria Globo";
+// novoEndPf.endComercial = true;
 
-novaPf.Endereco = novoEndPf;
+// novaPf.Endereco = novoEndPf;
 
 
-Console.WriteLine(@$"
-Nome: {novaPf.Nome}
-Endereço: {novaPf.Endereco.logradouro}, {novaPf.Endereco.numero}
-Maior de idade: {metodosPf.ValidarDataNasc(novaPf.dataNasc)}
-");
-
+// Console.WriteLine(@$"
+// Nome: {novaPf.Nome}
+// Endereço: {novaPf.Endereco.logradouro}, {novaPf.Endereco.numero}
+// Maior de idade: {metodosPf.ValidarDataNasc(novaPf.dataNasc)}
+// ");
 
 // Console.WriteLine($"Nome: {novaPf.Nome} Nome: {novaPf.Nome}"); // Interpolação
 // Console.WriteLine("Nome: " + novaPf.Nome + " Nome: " + novaPf.Nome); // Concatenação
+
+PessoaJuridica novaPj = new PessoaJuridica();
+PessoaJuridica metodosPj = new PessoaJuridica();
+Endereco novoEndPj = new Endereco();
+
+novaPj.Nome = "Nome Pj";
+novaPj.RazaoSocial = "Razão Social Pj";
+novaPj.Cnpj = "00000000000100";
+novaPj.Rendimento = 10000.5F;
+
+novoEndPj.logradouro = "Vereda dos Buritis";
+novoEndPj.numero = 3;
+novoEndPj.complemento = "Girassol Informática";
+novoEndPj.endComercial = true;
+
+novaPj.Endereco = novoEndPj;
+
+Console.WriteLine(@$"
+Nome: {novaPj.Nome}
+Razão Social: {novaPj.RazaoSocial}
+CNPJ: {novaPj.Cnpj}, Válido: {metodosPj.ValidarCnpj(novaPj.Cnpj)}
+Endereço: {novaPj.Endereco.logradouro}, Nº: {novaPj.Endereco.numero}
+Complemento: {novaPj.Endereco.complemento}
+");
